41e4a65476ff3b71822bb75a9b3db8cc66187dc5
IsaacWinner18 <winsamlab222@gmail.com>
2026-06-09 21:36:10 +0100

    added the dbconnect function.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
 import "dotenv";
 import cors from "cors";
+import DbConnect from "./lib/dbConnect";
 
 const app = express();
 app.use(express.json());
+
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, async () => {
+    await DbConnect();
+    console.log(`Server is running on port ${PORT}`)
+})
